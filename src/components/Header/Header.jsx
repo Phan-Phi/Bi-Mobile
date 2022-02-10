a3f1c3c49197dacfbf0439bf50d38ae3a4132da1
AnhPhi <phananhphi.pp@gmail.com>
2022-02-10 15:38:22 +0700

    push lan 16

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,22 @@ import { NavLink } from "react-router-dom";
 import "../../styles/header.scss";
 
 export default function Header() {
+  const BtnGioHang = () => {
+    const Hdisplay = document.querySelector(".headerDisplay");
+    const HgioHang = document.querySelector(".headerGioHang");
+    Hdisplay.style.display = "block";
+    HgioHang.style.transform = "translateX(0%)";
+    HgioHang.style.transition = "transform 0.6s";
+  };
+
+  const closeDisplay = () => {
+    const Hdisplay = document.querySelector(".headerDisplay");
+    const HgioHang = document.querySelector(".headerGioHang");
+    Hdisplay.style.display = "none";
+    HgioHang.style.transform = "translateX(100%)";
+    HgioHang.style.transition = "transform 0.6s";
+  };
+
   return (
     <header>
       <section>
@@ -52,7 +68,7 @@ export default function Header() {
                 </ul>
               </p>
             </div>
-            <div className="header__gioHang">
+            <div onClick={() => BtnGioHang()} className="header__gioHang">
               <p>
                 <i className="fas fa-shopping-cart" />
                 Giỏ hàng
@@ -64,6 +80,30 @@ export default function Header() {
           </div>
         </div>
       </section>
+      <div onClick={() => closeDisplay()} className="headerDisplay"></div>
+      <div className="headerGioHang">
+        <div className="headerGioHang_head">
+          <p>Giỏ hàng</p>
+          <p onClick={() => closeDisplay()}>
+            <i class="fas fa-times"></i>
+          </p>
+        </div>
+
+        <div className="headerGioHang_product"></div>
+
+        <div className="headerGioHang_footer">
+          <hr />
+          <div className="giaTien">
+            <p>Tổng tiền:</p>
+            <p className="so">
+              30000000<span>₫</span>
+            </p>
+          </div>
+          <div className="thanhToan">
+            <button>Thanh Toán</button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
--- src/components/SmartPhone/SmartPhone.jsx
+++ src/components/SmartPhone/SmartPhone.jsx
@@ -120,6 +120,15 @@ export default function SmartPhone(props) {
       {
         breakpoint: 1024,
         settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
