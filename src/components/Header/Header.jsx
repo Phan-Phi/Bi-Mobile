c9061f8fdf3ff2506be1ddef7d307ee5d05ea329
AnhPhi <phananhphi.pp@gmail.com>
2022-02-12 12:37:27 +0700

    push lan 17

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../../styles/header.scss";
-
+let menuConNav = true;
+let menuChauNavs = true;
 export default function Header() {
   const BtnGioHang = () => {
     const Hdisplay = document.querySelector(".headerDisplay");
@@ -19,10 +20,52 @@ export default function Header() {
     HgioHang.style.transition = "transform 0.6s";
   };
 
+  const openMenu = () => {
+    const Hdisplay = document.querySelector(".headerDisplay");
+    Hdisplay.style.display = "block";
+  };
+
+  const openMenuNavBar = () => {
+    const menuCon = document.querySelector(".menu-con");
+    const menuIcon = document.querySelector(".menu-icon");
+
+    if (menuConNav) {
+      menuIcon.innerHTML = `<i class="fas fa-angle-down"></i>`;
+      menuCon.style.display = "block";
+      menuCon.style.transition = "display 0.5s";
+      menuConNav = false;
+    } else {
+      menuIcon.innerHTML = `<i class="fas fa-angle-up"></i>`;
+      menuCon.style.display = "none";
+      menuCon.style.transition = "display 0.5s";
+      menuConNav = true;
+    }
+  };
+
+  const openMenuNavBarChau = () => {
+    const menuChau = document.querySelector(".menu-chau");
+    const menuChauIcon = document.querySelector(".menuChau-icon");
+    console.log("first");
+    if (menuChauNavs) {
+      console.log("first222");
+      menuChauIcon.innerHTML = `<i class="fas fa-angle-down"></i>`;
+      menuChau.style.display = "block";
+      menuChauNavs = false;
+    } else if (menuChauNavs == false) {
+      menuChauIcon.innerHTML = `<i class="fas fa-angle-up"></i>`;
+      menuChau.style.display = "none";
+      menuChauNavs = true;
+    }
+  };
+
   return (
     <header>
       <section>
         <div className="header">
+          <div className="navBar">
+            <i class="fas fa-bars" onClick={() => openMenu()}></i>
+          </div>
+
           <NavLink to="/">
             <img
               src="./img/BiMobile-logo-01.png"
@@ -37,6 +80,7 @@ export default function Header() {
               <i className="fas fa-search" />
             </p>
           </div>
+
           <div className="header__footer">
             <div className="header__phone">
               <i className="fas fa-phone-volume" />
@@ -70,8 +114,8 @@ export default function Header() {
             </div>
             <div onClick={() => BtnGioHang()} className="header__gioHang">
               <p>
-                <i className="fas fa-shopping-cart" />
-                Giỏ hàng
+                <i class="fas fa-shopping-bag"></i>
+                <span className="textGioHang">Giỏ hàng</span>
               </p>
               <div>
                 <span>0</span>
@@ -102,6 +146,116 @@ export default function Header() {
           <div className="thanhToan">
             <button>Thanh Toán</button>
           </div>
+        </div>
+      </div>
+
+      <div className="headerNavBar">
+        <div className="NavBar-Login">
+          <p className="iconUser">
+            <i class="far fa-user"></i>
+          </p>
+          <div className="iconText">
+            <p>
+              <NavLink to="#" class="login">
+                Đăng Nhập
+              </NavLink>
+            </p>
+            <p>
+              <NavLink to="#" class="login">
+                Đăng Ký
+              </NavLink>
+            </p>
+          </div>
+        </div>
+        <div className="NavBar-Menu">
+          <ul>
+            <li className="menu-content">
+              <a>
+                <p>Điện thoại</p>
+                <p onClick={() => openMenuNavBar()} className="menu-icon">
+                  <i class="fas fa-angle-up"></i>
+                </p>
+              </a>
+
+              <ul className="menu-con">
+                <li>
+                  <a href="#">
+                    <p>Apple</p>
+                    <p
+                      onClick={() => openMenuNavBarChau()}
+                      className="menuChau-icon"
+                    >
+                      <i class="fas fa-angle-up"></i>
+                    </p>
+                  </a>
+                  <ul className="menu-chau">
+                    <li>
+                      <a href="">iPhone 12 | 12 Pro | 12 Pro max</a>
+                    </li>
+                    <li>
+                      <a href="">iPhone 11 | 11 Pro | 11 Pro max</a>
+                    </li>
+                    <li>
+                      <a href="">iPhone Xs | Xs max</a>
+                    </li>
+                    <li>
+                      <a href="">iPhone X | Xr</a>
+                    </li>
+                    <li>
+                      <a href="">iPhone 8 | 8 Plus</a>
+                    </li>
+                    <li>
+                      <a href="">iPhone 7 | 7 Plus</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">
+                    <p>SamSung</p>
+                    <p
+                      onClick={() => openMenuNavBarChau()}
+                      className="menuChau-icon"
+                    >
+                      <i class="fas fa-angle-up"></i>
+                    </p>
+                  </a>
+                  <ul className="menu-chau">
+                    <li>
+                      <a href="">Galaxy Z Fold3 | Z Flip3 5G</a>
+                    </li>
+                    <li>
+                      <a href="">Galaxy S21 | S21 Plus | S21 Ultra</a>
+                    </li>
+                    <li>
+                      <a href="">Galaxy Note 20 | Note 20 Ultra 5G</a>
+                    </li>
+                    <li>
+                      <a href="">Galaxy Note 10 | Note 10 Plus</a>
+                    </li>
+                    <li>
+                      <a href="">Galaxy S10e | S10 | S10 5G</a>
+                    </li>
+                    <li>
+                      <a href="">Galaxy A Series</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+
+            <li className="menu-content">
+              <a>
+                <p>Dịch vụ sửa chữa</p>
+              </a>
+            </li>
+            <li className="menu-content">
+              <a>
+                <p>
+                  Sản phẩm yêu thích (<span>0</span>)
+                </p>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
